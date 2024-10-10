b94b7cc1d712c5556981fdeb6457376ca770fcd0
mheavey2 <mheavey2@gmail.com>
2024-10-10 12:43:12 +0100

    add functionality to change what ha ppens when timer expires. add dynamic countdown when event is selected.

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 console.log("JS loaded");
 
+// store page elements as variables
 const timerHeader = document.querySelector(".countdown-timer-heading");
 const countdownEvents = document.querySelector(".countdown-events-list");
 const countdownContainer = document.querySelector(".countdown-container");
@@ -7,21 +8,21 @@ const eventRadioButtons = document.querySelectorAll(
   "input[name=countdown-event]"
 );
 const timerEvent = document.querySelector("#countdown-timer-event");
-
-const gigDate = new Date("2024-10-27T18:00:00");
-const birthday = new Date("2024-10-26T00:00:00");
-const holidayStartDate = new Date("2024-10-19T00:00:00");
-const christmas = new Date("2024-12-25T00:00:00");
 const returnButton = document.querySelector("#return-btn");
-let currentEvent = "";
 returnButton.hidden = true;
 
-//function to calculate date difference
+//store various dates as an object
+const dates = {
+  gig: new Date("2024-10-10T12:05:00"),
+  birthday: new Date("2024-10-26T00:00:00"),
+  holidays: new Date("2024-10-19T00:00:00"),
+  christmas: new Date("2024-12-25T00:00:00"),
+};
+
+//function to calculate date difference and update the timer display
 function updateCountdownDisplay(target) {
   const currentDate = new Date();
   const difference = target - currentDate;
-  console.log(`diff: ${difference}`);
-  console.log(`current: ${currentEvent}`);
 
   const days = Math.floor(difference / (1000 * 60 * 60 * 24));
   const hours = Math.floor(
@@ -34,6 +35,13 @@ function updateCountdownDisplay(target) {
   document.querySelector("#countdown-hours").innerText = hours;
   document.querySelector("#countdown-minutes").innerText = minutes;
   document.querySelector("#countdown-seconds").innerText = seconds;
+
+  return {
+    days,
+    hours,
+    minutes,
+    seconds,
+  };
 }
 
 //function to capitalise event
@@ -42,47 +50,55 @@ function capitaliseEvent(event) {
   return result;
 }
 
-function getEvent(eventValue) {
-  switch (eventValue) {
-    case "christmas":
-      eventDate = christmas;
-      document.querySelector("#countdown-pronoun").hidden = true;
-      break;
-    case "gig":
-      eventDate = gigDate;
-      break;
-    case "birthday":
-      eventDate = birthday;
-      break;
-    case "holidays":
-      eventDate = holidayStartDate;
-      break;
-    default:
-      return eventDate;
-  }
-
-  timerEvent.innerText = capitaliseEvent(eventValue);
-  currentEvent = eventDate;
-  updateCountdownDisplay(eventDate);
-
-  returnButton.hidden = false;
+// function to handle when countdown is reached
+function timerExpired() {
+  document.querySelector("#countdown-timer-header-status").innerText =
+    "OVER!!!";
+  timerHeader.style.color = "#f05227";
+  timerHeader.style.fontSize = "3em";
 }
 
-//loop over radio buttons and add click event if clicked
+//loop over radio buttons and add click event when clicked.when button is clicked hide event list and show countdown timer. Update timer dynamically every second
 eventRadioButtons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.checked) {
       value = button.value;
-    }
+      countdownEvents.hidden = true;
+      countdownContainer.hidden = false;
 
-    countdownEvents.hidden = true;
-    countdownContainer.hidden = false;
-    getEvent(value);
+      function startInterval() {
+        if (dates.hasOwnProperty(value)) {
+          updateCountdownDisplay(dates[value]);
+        }
+        timerEvent.innerText = capitaliseEvent(value);
+
+        returnButton.hidden = false;
+      }
+      const timerInterval = setInterval(() => {
+        // startInterval();
+
+        if (
+          updateCountdownDisplay(dates[value]).days <= 0 &&
+          updateCountdownDisplay(dates[value]).hours <= 0 &&
+          updateCountdownDisplay(dates[value]).minutes <= 0 &&
+          updateCountdownDisplay(dates[value]).seconds <= 0
+        ) {
+          timerExpired();
+          clearInterval(timerInterval);
+        }
+      }, 1000);
+      startInterval();
+    }
   });
 });
 
-// handle return button click
+// handle return button click event
 returnButton.addEventListener("click", () => {
+  resetTimer();
+});
+
+//reset to defaults
+function resetTimer() {
   // hide timer and show event list
   countdownEvents.hidden = false;
   countdownContainer.hidden = true;
@@ -90,5 +106,7 @@ returnButton.addEventListener("click", () => {
   // clear current checked button
   eventRadioButtons.forEach((button) => {
     button.checked = false;
+    document.querySelector("#countdown-timer-header-status").innerText =
+      "ON!!!";
   });
-});
+}
